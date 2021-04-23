0289f1faeabfc0a17c3c02483cdb52bb475189db
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2021-04-23 19:02:59 +0530

    modified log stream name

--- logs.tf
+++ logs.tf
@@ -11,6 +11,6 @@ resource "aws_cloudwatch_log_group" "myapp_log_group" {
 }
 
 resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
-  name           = "production-ecs-log-group"
+  name           = "ecs_log_stream_${var.log_group_name}"
   log_group_name = aws_cloudwatch_log_group.myapp_log_group.name
 }
